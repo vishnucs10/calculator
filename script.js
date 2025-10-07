ef3b10bfe836566cd96839c8c116cf484e7c4409
vishnucs10 <130967841+vishnucs10@users.noreply.github.com>
2025-10-07 11:34:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function appendValue(value) {
+    document.getElementById('result').value += value;
+}
+
+function clearResult() {
+    document.getElementById('result').value = '';
+}
+
+function deleteLast() {
+    let result = document.getElementById('result').value;
+    document.getElementById('result').value = result.slice(0, -1);
+}
+
+function calculateResult() {
+    let result = document.getElementById('result').value;
+    try {
+        document.getElementById('result').value = eval(result);
+    } catch (error) {
+        document.getElementById('result').value = 'Error';
+    }
+}
